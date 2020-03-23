782c55e456ee5158959b4f18b770f6279c41eb1a
EC2 Default User <ec2-user@ip-172-31-37-170.eu-west-1.compute.internal>
2020-03-23 07:19:24 +0000

    corrections

--- src/demo.py
+++ src/demo.py
@@ -4,8 +4,10 @@ import sys
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s')
 
+logging.info('Fat Lambda Params ****STARTED****')
+
 try:
     logging.info(f'Fat Lambda started, param is: {sys.argv[1]}')
 except:
-    logging.info(f'Fat Lambda started but not input paramter supplied')
-# Do some work
+    logging.info(f'Fat Lambda started but no input paramter supplied')
+
